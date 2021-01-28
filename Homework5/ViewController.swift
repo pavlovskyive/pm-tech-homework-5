7f41bec1bd00ba7cb6a25fc2f154567228bc4791
Vsevolod Pavlovskyi <pavlovskyi.ve@gmail.com>
2021-01-28 13:15:34 +0200

    Initial Commit

---
+++ Homework5/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Homework5
+//
+//  Created by Vsevolod Pavlovskyi on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+}
